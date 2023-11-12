c0ab6060b4115581f6085f65a955f734876c30f8
Vlad <wbcom9543@gmail.com>
2023-11-12 19:15:45 +0200

    first commit

---
+++ Clouds/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FlyWeightL;
+
+namespace Clouds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+        Cloud[] clouds = new Cloud[20];
+        Graphics g;
+
+        ParamBigCloud p1 = new ParamBigCloud();
+        ParamMediumCloud p2 = new ParamMediumCloud();
+        ParamSmallCloud p3 = new ParamSmallCloud();
+    
+        Random r = new Random();
+        int[] ways = {0, 60, 120, 180, 240, 300, 360, 420};
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < clouds.Length; i++)
+            {  
+                switch (r.Next(0, 3)) {
+                    case 0: clouds[i] = new Cloud(r.Next(0, 450),  p1, r, ways); break;
+                    case 1: clouds[i] = new Cloud(r.Next(0, 450),  p2, r, ways); break;
+                    case 2: clouds[i] = new Cloud(r.Next(0, 450),  p3, r, ways); break;
+                }
+            }
+            timer1.Start();
+        }
+     
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            for (int i = 0; i < clouds.Length; i++)
+            {
+               g.FillRectangle(Brushes.White, clouds[i].x, clouds[i].y, clouds[i].param.w, clouds[i].param.h);
+               clouds[i].respawn();
+               g.DrawImage(clouds[i].param.picture, new Rectangle(clouds[i].x, clouds[i].y, clouds[i].param.w, clouds[i].param.h));
+            }  
+        }
+    }
+}
+
---
+++ FlyWeightL/Lehkovagovik.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyWeightL
+{
+   public class CloudParam
+    {
+        public int h;
+        public int w;
+        public Bitmap picture;
+        public int speed;
+        public int dir;
+    }
+    public class ParamBigCloud : CloudParam
+    {
+        public ParamBigCloud(){
+            picture = new Bitmap(@"D:\Images\BigCloud.png");
+            h = 60;
+            w = 70;
+            dir = 1;
+            speed = 2;
+        }
+    }
+    public class ParamMediumCloud : CloudParam
+    {
+        public ParamMediumCloud()
+        {
+            picture = new Bitmap(@"D:\Images\MediumCloud.png");
+            h = 30;
+            w = 60;
+            dir = 1;
+            speed = 4;
+        }
+    }
+    public class ParamSmallCloud : CloudParam
+    {
+        public ParamSmallCloud()
+        {
+            picture = new Bitmap(@"D:\Images\SmallCloud.png");
+            h = 20;
+            w = 40;
+            dir = 1;
+            speed = 6;
+        }
+    }
+    public class Cloud {
+        public int x;
+        public int y;
+        public Random rand;
+        public int y0;
+        public int[] ways;
+        public int dir;
+        public int speed;
+        public CloudParam param;
+
+        public Cloud(int y0, CloudParam param, Random r, int[] w){
+            this.y0 = y0;
+            this.dir = 1;
+            this.param = param;
+            this.rand = r;
+            this.x = r.Next(0, 450);
+            this.ways = w;
+            this.speed = param.speed;
+        }
+        public void respawn()
+        {
+
+            if (x > 850)
+            {
+
+                x = rand.Next(800, 835);
+                y0 = ways[rand.Next(0, ways.Length)];
+                dir *= -1;
+            }
+            else if (-50 > x) {
+                x = rand.Next(-50, 0);
+                y0 = ways[rand.Next(0, ways.Length)];
+                dir *= -1;
+            }
+            x += dir * speed;
+            y = y0;
+        }
+    }
+}
